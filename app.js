479a9b3539d48dcd4ac00ed719bdcbfe9f767396
Nam Bok Rausis <nambok@gmail.com>
2014-03-26 15:47:42 -0400

    added simple auth

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 var express = require('express');
 var http = require('http');
+// Authentication module.
+var auth = require('http-auth');
+var basic = auth.basic({
+		realm: "Vice HD"
+	}, function (username, password, callback) { // Custom authentication method.
+		callback(username === "vice" && password === "welcome");
+	}
+);
 
 var app = express();
+app.use(auth.connect(basic));
 
 [ 'settings'
 , 'middleware'
